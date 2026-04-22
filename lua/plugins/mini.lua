2ccfea6961b5ec33a4ba1c4a12e6e8528dd75db6
idli-dev <idlidev69@gmail.com>
2026-04-22 11:47:35 +0530

    removed git support ill add it later

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,13 +3,11 @@ vim.pack.add({
   "https://github.com/nvim-mini/mini.pairs",
   "https://github.com/nvim-mini/mini.surround",
   "https://github.com/windwp/nvim-ts-autotag",
-  "https://github.com/lewis6991/gitsigns.nvim"
 })
 
 require("mini.icons").setup()
 require("mini.pairs").setup()
 require("mini.surround").setup()
-require("gitsigns").setup()
 
 require("nvim-ts-autotag").setup({
   opts = {
